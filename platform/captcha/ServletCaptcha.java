e0e50ac11df50c876351b2d06cb9b915a1a4540d
Maxim Parygin <developer.mak@gmail.com>
2014-11-27 16:05:31 +0600

    Initial commit

--- platform/captcha/ServletCaptcha.java
+++ platform/captcha/ServletCaptcha.java
@@ -14,35 +14,37 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name="captcha", urlPatterns="/captcha")
 public final class ServletCaptcha extends HttpServlet {
 
-    private static String ATTRIBUTE = "check";
-    private static String HEX = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789_";
+    private static String ATTRIBUTE = "check"; // name of session attribute for store the value of captcha
+    private static String DICT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789_";  // dictionary
     private static int WIDTH = 128; // width (pixels)
     private static int HEIGHT = 48; // height (pixels)
     private static int NOISE = 400; // noise points
+    private static float FONT_SIZE = 28f; // font size
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
 
+        // image
         java.awt.image.BufferedImage bi = new java.awt.image.BufferedImage(WIDTH, HEIGHT, java.awt.image.BufferedImage.TYPE_INT_RGB);
-
         java.awt.Graphics2D g2 = (java.awt.Graphics2D) (bi.getGraphics());
-
         g2.setBackground(java.awt.Color.lightGray);
-        g2.clearRect(0, 0, 128, HEIGHT);
+        g2.clearRect(0, 0, WIDTH, HEIGHT);
         g2.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(java.awt.Color.BLACK);
-        g2.setFont(g2.getFont().deriveFont(28f));
+        g2.setFont(g2.getFont().deriveFont(FONT_SIZE));
 
+        // create text
         StringBuilder tmp = new StringBuilder();
         for (int i = 0; i < 5; i++) {
-            int p = (int) (Math.random() * HEX.length());
-            tmp.append(HEX.charAt(p));
+            int p = (int) (Math.random() * DICT.length());
+            tmp.append(DICT.charAt(p));
         }
         session.setAttribute(ATTRIBUTE, tmp.toString());
 
         g2.drawString(tmp.toString(), 16, 28);
 
+        // noise
         java.util.Random r = new java.util.Random();
         for (int i = 0; i < NOISE; i++) {
             g2.setColor(new java.awt.Color((int) (Math.random() * 0xffffff)));
@@ -51,6 +53,7 @@ public final class ServletCaptcha extends HttpServlet {
             g2.drawLine(x, y, x + 1, y + 1);
         }
 
+        // out
         response.setContentType("image/gif");
         javax.imageio.ImageIO.write(bi, "gif", response.getOutputStream());
     }
